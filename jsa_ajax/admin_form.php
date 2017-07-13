8bfac5793d9eb43c712dddb805b271c57358319e
upsarin <mvlad91@mail.ru>
2017-07-13 14:52:54 +0700

    tests with json type of data

---
+++ jsa_ajax/admin_form.php
@@ -0,0 +1,17 @@
+<div class="jsa_admin_form">
+    <div class="jsa_background"></div>
+    <div class="col-lg-8 col-md-8 col-sm-8 padbot30 jsa_admin_form-wrapper">
+        <div class="jsa_close">close</div>
+        <div class="contact_form">
+            <div id="note">Auth administrator form</div>
+            <div id="fields">
+                <form id="ajax-contact-form" action="">
+                    <input id="jsa_login" type="text" value="" placeholder="Login" style="width: 100%;height: 44px;margin: 0 30px 9px 0;padding: 12px 30px;line-height: 20px;font-size: 20px;color: #ffffff;text-shadow: none;border: 0;border-radius: 0;box-shadow: none;background: #55585a;"/>
+                    <input id="jsa_pass" type="password" value="" placeholder="Pass" style="width: 100%;height: 44px;margin: 0 30px 9px 0;padding: 12px 30px;line-height: 20px;font-size: 20px;color: #ffffff;text-shadow: none;border: 0;border-radius: 0;box-shadow: none;background: #55585a;"/>
+                    <input type="submit" value="Auth" />
+                    <div class="clear"></div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
